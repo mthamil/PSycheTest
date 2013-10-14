be487057eb0be030a1feda52ae66ee5140fb52fe
Matthew Hamilton <matthamilton@live.com>
2013-10-14 13:20:40 -0500

    Refactored discoverer to use manual injection.

--- PSycheTest.Runners.VisualStudio/VSTestDiscoverer.cs
+++ PSycheTest.Runners.VisualStudio/VSTestDiscoverer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
 using System.Linq;
@@ -18,13 +19,32 @@ namespace PSycheTest.Runners.VisualStudio
 	[Export(typeof(ITestDiscoverer))]
 	public class VSTestDiscoverer : ITestDiscoverer
 	{
+		/// <summary>
+		/// Initializes a new <see cref="VSTestDiscoverer"/>.
+		/// </summary>
+		[ImportingConstructor]
+		public VSTestDiscoverer()
+			: this(logger => new PowerShellTestDiscoverer(logger))
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new <see cref="VSTestDiscoverer"/>.
+		/// </summary>
+		/// <param name="discovererFactory">Creates test discoverers</param>
+		public VSTestDiscoverer(Func<ILogger, IPowerShellTestDiscoverer> discovererFactory)
+		{
+			_discovererFactory = discovererFactory;
+		}
+
 		/// <see cref="ITestDiscoverer.DiscoverTests"/>
 		public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
 		{
 			Channels.UnregisterAll();
 
-			var discoverer = new PowerShellTestDiscoverer(new VSLogger(logger));
+			var discoverer = _discovererFactory(new VSLogger(logger));
 			var tests = discoverer.Discover(sources
+				.Where(source => Path.GetExtension(source).Equals(TestScript.FileExtension, StringComparison.InvariantCultureIgnoreCase))
 				.Select(s => new FileInfo(s)))
 				.SelectMany(testScript => testScript.Tests)
 				.Select(test => _mapper.Map(test));
@@ -34,5 +54,6 @@ namespace PSycheTest.Runners.VisualStudio
 		}
 
 		private readonly TestMapper _mapper = new TestMapper();
+		private readonly Func<ILogger, IPowerShellTestDiscoverer> _discovererFactory;
 	}
 }
